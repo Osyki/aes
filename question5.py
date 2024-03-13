cda3a36418aea45ea7f5c71bab4f6069b9725a90
Rubio <jonrubio92@gmail.com>
2024-03-13 13:26:14 -0400

    Question 5

---
+++ question5.py
@@ -0,0 +1,10 @@
+from aes import AES, pad
+
+key = pad(b'this is my key') # need to pad to 16, 24, or 32 bytes
+iv = pad(b'this is my iv') # need to pad to 16 bytes
+
+cipher_text = AES(key).encrypt_cbc(b'this is my plain text for question 5', iv)
+print(cipher_text)
+
+plain_text = AES(key).decrypt_cbc(cipher_text, iv)
+print(plain_text)
